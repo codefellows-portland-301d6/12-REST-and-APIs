a13a0c8b0f81941c861387ade1abd69554e6da81
sdcaulley <stephanie.caulley@gmail.com>
2016-10-04 13:36:55 -0700

    added repo.js to html, did api call to codefellows repo

--- starter-code/scripts/models/repo.js
+++ starter-code/scripts/models/repo.js
@@ -6,6 +6,9 @@
   reposObj.requestRepos = function(callback) {
     /* TODO: How would you like to fetch your repos? Someone say AJAX?!
       Do not forget to call the callback! */
+    $.get('https://api.github.com/users/codefellows/repos', function(data) {
+      console.log('data from api: ', data);
+    });
   };
 
   reposObj.withTheAttribute = function(myAttr) {
