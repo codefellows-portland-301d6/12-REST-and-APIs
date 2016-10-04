6716f2bf4faa7f10a67d112470124a6c3fbb4aaa
willnickerson <wnickers@willamette.edu>
2016-10-04 13:39:30 -0700

    script tags and fecthed repos

--- starter-code/scripts/models/repo.js
+++ starter-code/scripts/models/repo.js
@@ -6,6 +6,9 @@
   reposObj.requestRepos = function(callback) {
     /* TODO: How would you like to fetch your repos? Someone say AJAX?!
       Do not forget to call the callback! */
+    $.get('https://api.github.com/users/risafletcher/repos').done(function(data) {
+      reposObj.allRepos = data;
+    });
   };
 
   reposObj.withTheAttribute = function(myAttr) {
