9150c9e86786158e904473bc8fa071694162b0b2
elhendricks <elhendricks@gmail.com>
2016-10-04 14:47:31 -0700

    added script tags and github token.  refactored request repos to ajax call

--- starter-code/scripts/models/repo.js
+++ starter-code/scripts/models/repo.js
@@ -6,10 +6,17 @@
   reposObj.requestRepos = function(callback) {
     /* TODO:DONE How would you like to fetch your repos? Someone say AJAX?!
       Do not forget to call the callback! */
-    $.get('https://api.github.com/users/TomTimmel/repos', function(data){
-      callback(data);
-    });
+    $.ajax({url: 'https://api.github.com/users/TomTimmel/repos',
+    success: function(data){
+      console.log('got to reposObj.requestRepos data: ', data);
+    },
+      headers: {Authorization: 'token '+ token},
+
+    }
+    );
   };
+
+  reposObj.requestRepos();
 
   reposObj.withTheAttribute = function(myAttr) {
     return reposObj.allRepos.filter(function(aRepo) {
