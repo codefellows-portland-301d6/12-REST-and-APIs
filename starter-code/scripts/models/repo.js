3f44891aaded718c34623d271d3c5704b2049dd5
dchasepdx <dchasepdx@gmail.com>
2016-10-04 13:51:32 -0700

    wrote ajax call, and wrote handlebars compile function

--- starter-code/scripts/models/repo.js
+++ starter-code/scripts/models/repo.js
@@ -4,8 +4,14 @@
   reposObj.allRepos = [];
 
   reposObj.requestRepos = function(callback) {
-    /* TODO: How would you like to fetch your repos? Someone say AJAX?!
+    /* TODO/DONE: How would you like to fetch your repos? Someone say AJAX?!
       Do not forget to call the callback! */
+    $.get('https://api.github.com/users/codefellows/repos').done(function(data){
+      data.forEach(function(obj){
+        reposObj.allRepos.push(obj);
+      });
+    });
+    callback();
   };
 
   reposObj.withTheAttribute = function(myAttr) {
--- starter-code/scripts/views/repoView.js
+++ starter-code/scripts/views/repoView.js
@@ -15,4 +15,6 @@
     Pass in some view function as a higher order callback, so our repos
     will render after the data is loaded. */
 
-});
+  reposObj.requestRepos();
+  module.repoView = repoView;
+})(window);
