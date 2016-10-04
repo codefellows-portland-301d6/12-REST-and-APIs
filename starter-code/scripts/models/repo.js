918e77822fc8dbab5f0149d4f12bd2753d507287
Christopher Bruner <chrbruner9@gmail.com>
2016-10-04 13:40:19 -0700

    Wrote in ajax call to github repo and finished adding callback, added all scripts tags in index, and completed Handlebars compiler

--- starter-code/scripts/models/repo.js
+++ starter-code/scripts/models/repo.js
@@ -6,6 +6,10 @@
   reposObj.requestRepos = function(callback) {
     /* TODO: How would you like to fetch your repos? Someone say AJAX?!
       Do not forget to call the callback! */
+    $.get('https://api.github.com/users/QuantumArchive/repos').done( function(data) {
+      reposObj.allRepos = data;
+      callback();
+    });
   };
 
   reposObj.withTheAttribute = function(myAttr) {
