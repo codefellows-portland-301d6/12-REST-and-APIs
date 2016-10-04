dd22ba29189f4acca508e6100d14e57c49157bbb
sdcaulley <stephanie.caulley@gmail.com>
2016-10-04 13:39:31 -0700

    added repo view to html file

--- starter-code/scripts/models/repo.js
+++ starter-code/scripts/models/repo.js
@@ -4,7 +4,7 @@
   reposObj.allRepos = [];
 
   reposObj.requestRepos = function(callback) {
-    /* TODO: How would you like to fetch your repos? Someone say AJAX?!
+    /* DONE: How would you like to fetch your repos? Someone say AJAX?!
       Do not forget to call the callback! */
     $.get('https://api.github.com/users/codefellows/repos', function(data) {
       console.log('data from api: ', data);
