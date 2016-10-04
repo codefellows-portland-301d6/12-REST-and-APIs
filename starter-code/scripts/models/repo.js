1b0d34270c8487463c734072f5a7458eeeb93484
TomTimmel <tom.t.timmel@gmail.com>
2016-10-04 15:32:02 -0700

    fixed iife, fixed handlebars function, fixed ajax function

--- starter-code/scripts/models/repo.js
+++ starter-code/scripts/models/repo.js
@@ -6,20 +6,19 @@
   reposObj.requestRepos = function(callback) {
     /* TODO:DONE How would you like to fetch your repos? Someone say AJAX?!
       Do not forget to call the callback! */
-    $.ajax({url: 'https://api.github.com/users/TomTimmel/repos',
-    success: function(data){
-
-      data.forEach(function(a) {
-        reposObj.allRepos.push(a);
-      });
-
-      console.log('test: ', reposObj.allRepos);
-      callback(reposObj.allRepos);
-    },
+    $.ajax({
+      url: 'https://api.github.com/users/TomTimmel/repos',
+      success: function(data){
+        data.forEach(function(a) {
+          reposObj.allRepos.push(a);
+        });
+        callback(reposObj.allRepos);
+      },
+      error: function(error){
+        console.log(error);
+      },
       headers: {Authorization: 'token '+ token},
-
-    }
-    );
+    });
   };
 
   reposObj.withTheAttribute = function(myAttr) {
--- starter-code/scripts/views/repoView.js
+++ starter-code/scripts/views/repoView.js
@@ -4,16 +4,16 @@
        Save the result of invoking Handlebars in this 'repoCompiler' variable
        that we will pass to the append method below. */
   var template = Handlebars.compile($('#repo-template').html());
-  var repoCompiler = template(this);  // Finish the Handlebars method here!
+  var repoCompiler = template;  // Finish the Handlebars method here!
 
   repoView.renderRepos = function() {
     $('#about ul').empty().append(
-      reposObj.withTheAttribute('name')  // TODO: experiment changing this attribute!
+      reposObj.withTheAttribute('name')  // TODO:Done experiment changing this attribute!
       .map(repoCompiler)
     );
   };
 /* TODO:DONE Call the function that loads (or 'requests') our repo data.
     Pass in some view function as a higher order callback, so our repos
     will render after the data is loaded. */
-  reposObj.requestRepos(repoView.renderRepos());
-});
+  reposObj.requestRepos(repoView.renderRepos);
+})(window);
