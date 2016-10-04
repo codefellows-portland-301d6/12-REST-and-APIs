34249e3d36cbff2fd069a511c17316bbb5ec9733
Mugsy Carter <mugsycarter@gmail.com>
2016-10-04 13:26:41 -0700

    added scripts to html.  Built the skeleton of our ajax call.

--- starter-code/scripts/models/repo.js
+++ starter-code/scripts/models/repo.js
@@ -6,6 +6,18 @@
   reposObj.requestRepos = function(callback) {
     /* TODO: How would you like to fetch your repos? Someone say AJAX?!
       Do not forget to call the callback! */
+    $.ajax({
+      url: 'https://api.github.com/users/mugsycarter/repos',
+      password: token,
+      success: function(data, status, xhr){
+        console.log(data);
+      },
+      error: function(xhr, settings, error){
+        console.log(error);
+      }
+    });
+
+
   };
 
   reposObj.withTheAttribute = function(myAttr) {
