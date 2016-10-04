22f57376b84a4d4913258bae368ace0c312aff76
dchasepdx <dchasepdx@gmail.com>
2016-10-04 13:51:32 -0700

    wrote ajax call, and wrote handlebars compile function

--- starter-code/scripts/views/repoView.js
+++ starter-code/scripts/views/repoView.js
@@ -1,9 +1,9 @@
 (function(module) {
   var repoView = {};
-  /* TODO: Let's compile our new template!
+  /* TODO/DONE: Let's compile our new template!
        Save the result of invoking Handlebars in this 'repoCompiler' variable
        that we will pass to the append method below. */
-  var repoCompiler;  // Finish the Handlebars method here!
+  var repoCompiler = Handlebars.compile($('#repo-template').text());  // Finish the Handlebars method here!
 
   repoView.renderRepos = function() {
     $('#about ul').empty().append(
@@ -15,4 +15,6 @@
     Pass in some view function as a higher order callback, so our repos
     will render after the data is loaded. */
 
-});
+  reposObj.requestRepos();
+  module.repoView = repoView;
+})(window);
