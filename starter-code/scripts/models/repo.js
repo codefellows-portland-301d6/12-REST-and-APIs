e1b3a0c121f3d6ec5062a50760c241e4ef6b5516
JPGothro <jpgothro@gmail.com>
2016-10-07 10:57:37 -0700

    Class 12 Lab on my own

--- starter-code/scripts/models/repo.js
+++ starter-code/scripts/models/repo.js
@@ -6,6 +6,22 @@
   reposObj.requestRepos = function(callback) {
     /* TODO: How would you like to fetch your repos? Someone say AJAX?!
       Do not forget to call the callback! */
+    var request = $.ajax({
+      url: 'https://api.github.com/user/repos',
+      method: 'GET',
+      data: 'access_token=' + githubAccessToken
+    })
+    .done(function(rawData) {
+      rawData.forEach(function(item) {
+        // Store the Repo data from JSON
+        reposObj.allRepos.push(item);
+      });
+      callback();
+    })
+    .fail(function(jqxhr, status) {
+      console.log('ajax call failed: ', status);
+      alert('ajax call failed: ' + status);
+    });
   };
 
   reposObj.withTheAttribute = function(myAttr) {
